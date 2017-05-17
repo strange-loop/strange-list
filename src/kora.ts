ce58c384f244b3c756328c5211850ef274b4a9c1
Dax Ravi <d@ironbay.digital>
2017-05-17 17:07:32 -0400

    Production url

--- src/kora.ts
+++ src/kora.ts
@@ -1,4 +1,4 @@
 import { MemoryStore, Store as StoreBase, Kora as KoraBase } from '@ironbay/kora'
 
 export const Store = new MemoryStore() as StoreBase
-export const Kora = new KoraBase('ws://localhost:12000/socket', Store)
+export const Kora = new KoraBase('ws://clank.ironbay.digital/socket', Store)
